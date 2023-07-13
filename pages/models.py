ee76fdc4863c669a8eaca226c28443f0068686b1
luisSantamariaCOL <luisestebansantamaria@outlook.com>
2023-07-13 15:41:13 -0500

    new static files

--- pages/models.py
+++ pages/models.py
@@ -15,4 +15,4 @@ class Team(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.first_name + " " + self.last_name
+        return self.first_name + " " + self.last_name + " (" + self.designation + ")"
