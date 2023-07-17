ad278ed5071cf65e586e3be253fcf66afbbbf957
luisSantamariaCol <luisestebansantamaria@outlook.com>
2023-07-17 13:59:09 -0500

    all cars showed in home

--- pages/views.py
+++ pages/views.py
@@ -8,9 +8,12 @@ def home(request):
 
     featured_cars = Car.objects.order_by('-created_date').filter(is_featured=True)
 
+    all_cars = Car.objects.order_by('-created_date')
+
     data = {
         'teams': teams,
         'featured_cars': featured_cars,
+        'all_cars': all_cars,
     }
 
     return render(request, 'pages/home.html', data)
