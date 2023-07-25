7daa5278743b2d06748e007fe23af5a18403b71a
luisSantamariaCol <luisestebansantamaria@outlook.com>
2023-07-25 14:43:20 -0500

    contact admin customization

--- contacts/models.py
+++ contacts/models.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 
 # Create your models here.
-class Contacts(models.Model):
+class Contact(models.Model):
     first_name = models.CharField(help_text="First Name", max_length=100)
     last_name = models.CharField(max_length=100)
     car_id = models.IntegerField()
