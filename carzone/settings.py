ced700783800b7c81b340919c38ba1edb808182d
luisSantamariaCol <luisestebansantamaria@outlook.com>
2023-08-01 11:06:42 -0500

    securing email password

--- carzone/settings.py
+++ carzone/settings.py
@@ -173,6 +173,6 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'estebanmillo700@gmail.com'
-EMAIL_HOST_PASSWORD = 'cmvnbesueixltwch'  #gdlmrzzpbrqaislk # config('EMAIL_PASSWORD')
+EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD') #gdlmrzzpbrqaislk # 
 EMAIL_USE_TLS = True
 
